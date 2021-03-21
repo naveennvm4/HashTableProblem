d5948d6a5e44cbfb3870fedc9b8a93a9017999ca
naveennvm4 <naveennvm4@gmail.com>
2021-03-21 08:42:17 +0530

    [Naveen]Added:Ability to find Frequency of word in Paragraph

--- src/test/java/MyHashMapTest.java
+++ src/test/java/MyHashMapTest.java
@@ -17,6 +17,23 @@ public class MyHashMapTest {
         System.out.println(myHashMap);
         Assertions.assertEquals(2, frequency);
     }
+    @Test
+    public void givenAParagraphWhenWordsAreAddedToListShouldReturnWordFrequency() {
+        String sentence = "Paranoids are not paranoid because " +
+                "they are paranoid but because they keep putting themselves " +
+                "deliberately into paranoid avoidable situations";
+        MyHashMap<String, Integer> myHashMap = new MyHashMap();
+        String[] words = sentence.toLowerCase().split(" ");
+        for (String word : words) {
+            Integer value = myHashMap.get(word);
+            if (value == null) value = 1;
+            else value = value + 1;
+            myHashMap.add(word, value);
+        }
+        int frequency = myHashMap.get("paranoid");
+        System.out.println(myHashMap);
+        Assertions.assertEquals(3, frequency);
+    }
 }
 
 
